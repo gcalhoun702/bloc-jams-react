d8134119182fdf666b63187efc0d4ca2038f85c9
Gregg Calhoun <greggcalhoun@Gregs-MacBook-Air.local>
2019-01-05 13:12:42 -0800

    checkpoint player bar complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -76,7 +77,13 @@ class Album extends Component {
     });
   }
 
-
+  handlePrevClick() {
+  const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+  const newIndex = Math.max(0, currentIndex -1);
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play();
+}
 
   render() {
     console.log(this.state.album.songs)
@@ -109,6 +116,12 @@ class Album extends Component {
 }
         </tbody>
       </table>
+      <PlayerBar
+               isPlaying={this.state.isPlaying}
+               currentSong={this.state.currentSong}
+               handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+               handlePrevClick={() => this.handlePrevClick()}
+            />
 
     </section>
     );
